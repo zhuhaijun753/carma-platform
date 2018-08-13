38cc71b1df3dbc2f5623ec2dfb5e1980d1c5de73
aphelyon <mhc6kp@virginia.edu>
2018-08-13 15:51:08 -0400

    testing local changes

---
+++ build_parse.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+git add .
+guidance_version='cat ./carmajava/guidance/src/main/java/gov/dot/fhwa/saxton/carma/guidance/CarmaVersion.java'
+major=$($guidance_version | grep 'int major' | grep -o '[0-9]*')
+intermediate=$($guidance_version | grep 'int intermediate' | grep -o '[0-9]*')
+minor=$($guidance_version | grep 'int minor' | grep -o '[0-9]*')
+current_commit_short_hash=$(git rev-parse --short HEAD)
+cd carmajava
+hide=$(./gradlew :guidance:writeVersionFile)
+cd ./guidance/src/main/resources/
+version_name=$(sed -n 1p version)
+echo "The predicted build name is "$major'.'$intermediate'.'$minor'.'$version_name
+echo "The shorthand sha hash of the latest commit of the branch you are currently on is "$current_commit_short_hash
